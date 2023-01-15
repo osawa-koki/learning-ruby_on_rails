d9d7199b337752bf446bc49267fa7ab0cb57c8d6
osawa-koki <github@osawa.dev>
2023-01-15 20:49:36 +0900

    不要なコード消した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   # get "URL" => "コントローラー名#アクション名"
-  get 'greeting/sayhello'
-  get 'greeting/saygoodmorning'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
