a0c32c3ac9b81a635055ea5ca8ab4e2cee474a40
osawa-koki <github@osawa.dev>
2023-01-15 23:13:05 +0900

    投稿エリア。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # get "URL" => "コントローラー名#アクション名"
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
